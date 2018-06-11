fd04c3f30f234bb2cff549e8ebd9fe402acf1f78
Kalvin <kalvinlim11@gmail.com>
2018-06-11 14:02:31 -0700

    Created User domain object with validation

---
+++ src/main/java/com/activemeasure/domain/User.java
@@ -0,0 +1,69 @@
+package com.activemeasure.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min=8)
+    private String password;
+
+    @Column(columnDefinition="DATETIME")
+    private LocalDate createTime;
+
+    @Column(columnDefinition="DATETIME")
+    private LocalDate updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDate getCreateTime() {
+        return createTime;
+    }
+
+    public LocalDate getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCreateTime(LocalDate createTime) {
+        this.createTime = createTime;
+    }
+
+    public void setUpdateTime(LocalDate updateTime) {
+        this.updateTime = updateTime;
+    }
+}
