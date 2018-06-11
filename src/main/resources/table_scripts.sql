cb835341aa71b586db422497849e6c661fcf2503
Kalvin <kalvinlim11@gmail.com>
2018-06-11 14:28:00 -0700

    added sql scripts and application properties db connection stub

---
+++ src/main/resources/table_scripts.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `notes`.`Users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `create_time` DATETIME NULL,
+  `last_update_time` DATETIME NULL,
+  PRIMARY KEY (`id`, `email`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
+
+
+CREATE TABLE `notes`.`notes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(50) NOT NULL,
+  `note` VARCHAR(1000) NULL,
+  `create_time` DATETIME NULL,
+  `last_update_time` DATETIME NULL,
+  `email` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
+
+INSERT INTO `notes`.`Users` (`email`, `password`, `create_time`, `last_update_time`)
+VALUES ('kalvin@kalvin.com', 'password', '\'2018-06-11 00:00:00\'', '\'2018-06-11 00:00:00\'');
